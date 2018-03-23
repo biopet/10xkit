062e2d21351b6bd03101f06c62a97384cc400df5
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-03-23 09:39:22 +0100

    Run per contig

--- src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
+++ src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
@@ -5,12 +5,15 @@ import java.io.{File, PrintWriter}
 import htsjdk.samtools.reference.IndexedFastaSequenceFile
 import nl.biopet.utils.tool.{AbstractOptParser, ToolCommand}
 import nl.biopet.utils.io
-import nl.biopet.utils.ngs.fasta
+import nl.biopet.utils.ngs.bam
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.bdgenomics.adam.models.ReferenceRegion
 import org.bdgenomics.adam.rdd.read.AlignmentRecordRDD
 import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.adam.sql.{Genotype, Variant}
+
+import scala.collection.JavaConversions._
 
 object CellVariantcaller extends ToolCommand[Args] {
   def argsParser: AbstractOptParser[Args] = new ArgsParser(this)
@@ -30,62 +33,66 @@ object CellVariantcaller extends ToolCommand[Args] {
     logger.info(
       s"Context is up, see ${sparkSession.sparkContext.uiWebUrl.getOrElse("")}")
 
+    val dict = bam.getDictFromBam(cmdArgs.inputFile)
     val correctCells =
       sc.broadcast(io.getLinesFromFile(cmdArgs.correctCells).toSet)
 
-    val allReads: AlignmentRecordRDD =
-      sc.loadBam(cmdArgs.inputFile.getAbsolutePath)
+    val filteredReads = for (contig <- dict.getSequences) yield {
+      val contigName = contig.getSequenceName
+      val contigRegion = ReferenceRegion(contig.getSequenceName, 1, contig.getSequenceLength)
+      val contigReads: AlignmentRecordRDD =
+        sc.loadIndexedBam(cmdArgs.inputFile.getAbsolutePath, contigRegion)
 
-    val filteredReads = allReads.rdd
-      .filter(r => !r.getDuplicateRead && r.getReadMapped)
-      .flatMap { read =>
-        read.getAttributes
-          .split("\t")
-          .find(_.startsWith(cmdArgs.sampleTag + ":"))
-          .map(_.split(":")(2))
-          .filter(correctCells.value.contains)
-          .map(
-            SampleRead(_,
-                       read.getContigName,
-                       read.getStart + 1,
-                       read.getEnd,
-                       read.getSequence.getBytes,
-                       read.getQual.getBytes,
-                       read.getCigar,
-                       !read.getReadNegativeStrand))
-      }
-      .flatMap(_.sampleBases.filter(_.avgQual.exists(_ >= cmdArgs.minBaseQual)))
-      .groupBy(x => (x.sample, x.contig, x.pos))
-      .mapPartitions { it =>
-        val fastaReader = new IndexedFastaSequenceFile(cmdArgs.reference)
-        it.map { case ((sample, contig, pos),list) =>
-          val end = pos + list.map(_.delBases).max
-          val refAllele = fastaReader.getSubsequenceAt(contig, pos, end).getBaseString
-          val alleles = list.groupBy(x => (x.allele, x.delBases)).map { case ((allele, delBases), bases) =>
-            val newAllele = if (delBases > 0 || !refAllele.startsWith(allele)) {
-              if (allele.length == refAllele.length || delBases > 0) allele
-              else new String(refAllele.zipWithIndex.map(x => allele.lift(x._2).getOrElse(x._1)).toArray)
-            } else refAllele
-            val total = bases.size
-            val forward = bases.count(_.strand)
-            val reverse = total - forward
-            AlleleCount(newAllele, forward, reverse, newAllele == refAllele)
-          }.toList
-          SampleVariant(sample, contig, pos, if (alleles.exists(_.allele == refAllele)) alleles else AlleleCount(refAllele, 0, 0, true) :: alleles)
+      contigReads.rdd
+        .filter(r => !r.getDuplicateRead && r.getReadMapped)
+        .flatMap { read =>
+          read.getAttributes
+            .split("\t")
+            .find(_.startsWith(cmdArgs.sampleTag + ":"))
+            .map(_.split(":")(2))
+            .filter(correctCells.value.contains)
+            .map(
+              SampleRead(_,
+                read.getContigName,
+                read.getStart + 1,
+                read.getEnd,
+                read.getSequence.getBytes,
+                read.getQual.getBytes,
+                read.getCigar,
+                !read.getReadNegativeStrand))
         }
+        .flatMap(_.sampleBases.filter(_.avgQual.exists(_ >= cmdArgs.minBaseQual))) //TODO: reduce
+        .groupBy(x => (x.sample, x.pos))
+        .mapPartitions { it =>
+          val fastaReader = new IndexedFastaSequenceFile(cmdArgs.reference)
+          it.map { case ((sample, pos),list) =>
+            val end = pos + list.map(_.delBases).max
+            val refAllele = fastaReader.getSubsequenceAt(contigName, pos, end).getBaseString
+            val alleles = list.groupBy(x => (x.allele, x.delBases)).map { case ((allele, delBases), bases) =>
+              val newAllele = if (delBases > 0 || !refAllele.startsWith(allele)) {
+                if (allele.length == refAllele.length || delBases > 0) allele
+                else new String(refAllele.zipWithIndex.map(x => allele.lift(x._2).getOrElse(x._1)).toArray)
+              } else refAllele
+              val total = bases.size
+              val forward = bases.count(_.strand)
+              val reverse = total - forward
+              AlleleCount(newAllele, forward, reverse, newAllele == refAllele)
+            }.toList
+            SampleVariant(sample, contigName, pos, if (alleles.exists(_.allele == refAllele)) alleles else AlleleCount(refAllele, 0, 0, true) :: alleles)
+          }
+        }
+        .groupBy(x => x.pos).map { case (pos, list) =>
+        VariantCall.from(list.toList, contigName, pos)
+      }
+        .filter(_.hasNonReference)
+        .filter (_.altDepth >= cmdArgs.minAlternativeDepth)
+        .filter (_.totalDepth >= cmdArgs.minTotalDepth)
+        .filter(_.minSampleAltDepth(cmdArgs.minCellAlternativeDepth))
+        .toDS()
     }
-      .groupBy(x => (x.contig, x.pos)).map { case ((contig, pos), list) =>
-      VariantCall.from(list.toList, contig, pos)
-    }
-      .filter(_.hasNonReference)
-      .filter (_.altDepth >= cmdArgs.minAlternativeDepth)
-      .filter (_.totalDepth >= cmdArgs.minTotalDepth)
-      .filter(_.minSampleAltDepth(cmdArgs.minCellAlternativeDepth))
-      .toDS()
-      //.cache()
 
     //val writer = new PrintWriter(new File(cmdArgs.outputDir, "counts.tsv"))
-    val df = filteredReads.toDF().map(_.toString()).write.csv(new File(cmdArgs.outputDir, "counts.tsv").getAbsolutePath)
+    filteredReads.reduce(_.union(_)).toDF().map(_.toString()).write.csv(new File(cmdArgs.outputDir, "counts.tsv").getAbsolutePath)
 //      .groupBy("sample", "contig", "pos", "allele", "delBases")
 //      .count()
 //    writer.println(df.columns.mkString("#","\t", ""))
