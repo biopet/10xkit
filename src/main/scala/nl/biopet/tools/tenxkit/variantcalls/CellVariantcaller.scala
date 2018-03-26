e26da43268406feebf2717feac572880ea251713
pjvan_thof <pjrvanthof@gmail.com>
2018-03-26 13:11:54 +0200

    Adding simple aggregation

--- src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
+++ src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
@@ -42,12 +42,14 @@ object CellVariantcaller extends ToolCommand[Args] {
     val correctCellsMap = sc.broadcast(correctCells.value.zipWithIndex.toMap)
     val minBaseQual = sc.broadcast(cmdArgs.minBaseQual)
 
-    val contigs = dict.getSequences.map(c => ReferenceRegion(c.getSequenceName, 1, c.getSequenceLength))
+    val contigs = dict.getSequences.map(c =>
+      ReferenceRegion(c.getSequenceName, 1, c.getSequenceLength))
 
     val allReads = sc.loadIndexedBam(cmdArgs.inputFile.getAbsolutePath, contigs)
 
     val filteredReads = for (contig <- contigs) yield {
       val contigName = contig.referenceName
+      logger.info(s"Start on contig '$contigName'")
       sc.setJobGroup(s"Contig: $contigName", s"Contig: $contigName")
 //      val contigReads: AlignmentRecordRDD =
 //        sc.loadIndexedBam(cmdArgs.inputFile.getAbsolutePath, contigRegion)
@@ -70,45 +72,57 @@ object CellVariantcaller extends ToolCommand[Args] {
           .filter(_._2.avgQual.exists(_ >= minBaseQual.value))
       }
 
-      val ds = bases
-        .groupByKey()
-        .mapPartitions { it =>
-          val fastaReader = new IndexedFastaSequenceFile(cmdArgs.reference)
-          it.map {
-            case (samplePos, list) =>
-              val end = samplePos.position + list.map(_.delBases).max
-              val refAllele = fastaReader
-                .getSubsequenceAt(contigName, samplePos.position, end)
-                .getBaseString
-              val alleles = list
-                .groupBy(x => (x.allele, x.delBases))
-                .map {
-                  case ((allele, delBases), bases) =>
-                    val newAllele =
-                      if (delBases > 0 || !refAllele.startsWith(allele)) {
-                        if (allele.length == refAllele.length || delBases > 0)
-                          allele
-                        else
-                          new String(refAllele.zipWithIndex
-                            .map(x => allele.lift(x._2).getOrElse(x._1))
-                            .toArray)
-                      } else refAllele
-                    val total = bases.size
-                    val forward = bases.count(_.strand)
-                    val reverse = total - forward
-                    AlleleCount(newAllele,
-                                forward,
-                                reverse,
-                                newAllele == refAllele)
-                }
-                .toList
-              val newAlleles =
-                if (alleles.exists(_.allele == refAllele)) alleles
-                else AlleleCount(refAllele, 0, 0, true) :: alleles
-              samplePos.position -> SampleVariant(samplePos.sample, newAlleles)
-          }
+      val bla = bases.aggregateByKey(Map[Key, Value]())({ case (a, b) =>
+        val key = Key(b.allele, b.delBases)
+        a.get(key) match {
+          case Some(count) if b.strand => a + (key -> count.copy(forward = count.forward + 1))
+          case Some(count) => a + (key -> count.copy(reverse = count.reverse + 1))
+          case _ if b.strand => a + (key -> Value(forward = 1))
+          case _ => a + (key -> Value(reverse = 1))
         }
-        .groupByKey()
+      }, { case (a, b) =>
+          val keys = a.keySet ++ b.keySet
+          keys.map { key =>
+            (a.get(key), b.get(key)) match {
+              case (Some(x), Some(y)) => key -> Value(x.forward + y.forward, x.reverse + y.reverse)
+              case (Some(x), _) => key -> x
+              case (_, Some(y)) => key -> y
+            }
+          }.toMap
+      }).mapPartitions { it =>
+        val fastaReader = new IndexedFastaSequenceFile(cmdArgs.reference)
+        it.map {
+          case (samplePos, list) =>
+            val end = samplePos.position + list.map(_._1.delBases).max
+            val refAllele = fastaReader
+              .getSubsequenceAt(contigName, samplePos.position, end)
+              .getBaseString
+            val alleles = list
+              .map {
+                case (key, bases) =>
+                  val newAllele =
+                    if (key.delBases > 0 || !refAllele.startsWith(key.allele)) {
+                      if (key.allele.length == refAllele.length || key.delBases > 0)
+                        key.allele
+                      else
+                        new String(refAllele.zipWithIndex
+                          .map(x => key.allele.lift(x._2).getOrElse(x._1))
+                          .toArray)
+                    } else refAllele
+                  AlleleCount(newAllele,
+                    bases.forward,
+                    bases.reverse,
+                    newAllele == refAllele)
+              }
+              .toList
+            val newAlleles =
+              if (alleles.exists(_.allele == refAllele)) alleles
+              else AlleleCount(refAllele, 0, 0, true) :: alleles
+            samplePos.position -> SampleVariant(samplePos.sample, newAlleles)
+        }
+      }
+
+      val ds = bla.groupByKey()
         .map {
           case (pos, list) =>
             VariantCall.from(list.toList, contigName, pos)
@@ -133,6 +147,9 @@ object CellVariantcaller extends ToolCommand[Args] {
 
     logger.info("Done")
   }
+
+  case class Value(forward: Int = 0, reverse: Int = 0)
+  case class Key(allele: String, delBases: Int)
 
   def descriptionText: String =
     """
