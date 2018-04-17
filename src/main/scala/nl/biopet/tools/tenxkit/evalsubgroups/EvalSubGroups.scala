77fa49229701ea77ae3edf4b3d0eb5e8738f014a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-04-17 11:05:23 +0200

    Added docs to EvalSubGroups

--- src/main/scala/nl/biopet/tools/tenxkit/evalsubgroups/EvalSubGroups.scala
+++ src/main/scala/nl/biopet/tools/tenxkit/evalsubgroups/EvalSubGroups.scala
@@ -2,7 +2,7 @@ package nl.biopet.tools.tenxkit.evalsubgroups
 
 import java.io.File
 
-import nl.biopet.tools.tenxkit.DistanceMatrix
+import nl.biopet.tools.tenxkit.{DistanceMatrix, TenxKit}
 import nl.biopet.utils.tool.ToolCommand
 
 import scala.io.Source
@@ -68,16 +68,21 @@ object EvalSubGroups extends ToolCommand[Args] {
 
   def descriptionText: String =
     """
-      |
+      |This tool will compare all given groups.
+      |The histogram for a Correct match should be a steep histogram starting at 0.
+      |When multiple peaks are seen this might be a mixture of samples.
     """.stripMargin
 
   def manualText: String =
     """
-      |
+      |This tool will require the complete distance matrix and a list of all cells per group.
+      |It will generate a histogram for each combination of groups.
     """.stripMargin
 
   def exampleText: String =
-    """
+    s"""
+      |Default example:
+      |${TenxKit.example("EvalSubGroups", "-o", "<output directory>", "-i", "<distance matrix>", "-g", "<group 1>=<group 1 file>", "-g", "<group 2>=<group 2 file>", "-g", "<<group 3>=group 3 file>")}
       |
     """.stripMargin
 
