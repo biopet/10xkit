95a5715b7ac6eb49725e89217920da17e9d29bb4
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-04-21 16:19:21 +0200

    Added test to downsampling

--- src/test/scala/nl/biopet/tools/tenxkit/mergebams/MergeBamsTest.scala
+++ src/test/scala/nl/biopet/tools/tenxkit/mergebams/MergeBamsTest.scala
@@ -21,8 +21,13 @@
 
 package nl.biopet.tools.tenxkit.mergebams
 
+import java.io.File
+
+import htsjdk.samtools.{SamReaderFactory, ValidationStringency}
 import nl.biopet.utils.test.tools.ToolTest
-import org.testng.annotations.Test
+import org.testng.annotations.{DataProvider, Test}
+
+import scala.collection.JavaConversions._
 
 class MergeBamsTest extends ToolTest[Args] {
   def toolCommand: MergeBams.type = MergeBams
@@ -86,5 +91,101 @@ class MergeBamsTest extends ToolTest[Args] {
           "bam3=<file>"
         ))
     }.getMessage shouldBe "requirement failed: Not all given barcode files have a bam file"
+  }
+
+  @Test
+  def testDefault(): Unit = {
+    val outputBam = File.createTempFile("merge.", ".bam")
+    outputBam.deleteOnExit()
+    val outputBarcodes = File.createTempFile("merge.", ".txt")
+    outputBarcodes.deleteOnExit()
+    MergeBams.main(
+      Array(
+        "--inputBam",
+        s"bam1=${resourcePath("/wgs2.realign.bam")}",
+        "--inputBarcode",
+        s"bam1=${resourcePath("/wgs2.readgroups.txt")}",
+        "--inputBam",
+        s"bam2=${resourcePath("/wgs2.realign.bam")}",
+        "--inputBarcode",
+        s"bam2=${resourcePath("/wgs2.readgroups.txt")}",
+        "--outputBam",
+        s"$outputBam",
+        "--outputBarcodes",
+        s"$outputBarcodes",
+        "--sampleTag",
+        "RG"
+      ))
+
+    val reader = SamReaderFactory
+      .makeDefault()
+      .validationStringency(ValidationStringency.SILENT)
+      .open(outputBam)
+    val it1 = reader.iterator()
+    it1.size shouldBe 40000
+    it1.close()
+
+    val it2 = reader.iterator()
+    val cells =
+      it2.flatMap(x => Option(x.getAttribute("RG")).map(_.toString)).toSet
+    cells shouldBe Set("bam1-wgs2-lib2",
+                       "bam1-wgs2-lib1",
+                       "bam2-wgs2-lib2",
+                       "bam2-wgs2-lib1")
+    it2.close()
+    reader.close()
+  }
+
+  @DataProvider(name = "fractions")
+  def fractions: Array[Array[Any]] =
+    Array(Array(0.1),
+          Array(0.2),
+          Array(0.3),
+          Array(0.4),
+          Array(0.5),
+          Array(0.6),
+          Array(0.7),
+          Array(0.8),
+          Array(0.9),
+          Array(1.0))
+
+  @Test(dataProvider = "fractions")
+  def testDownsample(fraction: Double): Unit = {
+    val outputBam = File.createTempFile("merge.", ".bam")
+    outputBam.deleteOnExit()
+    val outputBarcodes = File.createTempFile("merge.", ".txt")
+    outputBarcodes.deleteOnExit()
+    MergeBams.main(
+      Array(
+        "--inputBam",
+        s"bam1=${resourcePath("/wgs2.realign.bam")}",
+        "--inputBarcode",
+        s"bam1=${resourcePath("/wgs2.readgroups.txt")}",
+        "--inputBam",
+        s"bam2=${resourcePath("/wgs2.realign.bam")}",
+        "--inputBarcode",
+        s"bam2=${resourcePath("/wgs2.readgroups.txt")}",
+        "--outputBam",
+        s"$outputBam",
+        "--outputBarcodes",
+        s"$outputBarcodes",
+        "--sampleTag",
+        "RG",
+        "--downsampleFraction",
+        fraction.toString
+      ))
+
+    val reader = SamReaderFactory
+      .makeDefault()
+      .validationStringency(ValidationStringency.SILENT)
+      .open(outputBam)
+    val it1 = reader.iterator()
+    val aim = 40000 * fraction
+    val reads = it1.size
+    reads >= (aim - (aim / 10)) shouldBe true
+    reads <= (aim + (aim / 10)) shouldBe true
+    it1.close()
+
+    reader.close()
   }
 }
