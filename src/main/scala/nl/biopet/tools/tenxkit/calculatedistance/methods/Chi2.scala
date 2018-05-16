e06c2581b7a98970008e0da94e11d99ee5ec7e4a
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-05-16 09:31:15 +0200

    Added implementation of chi2

---
+++ src/main/scala/nl/biopet/tools/tenxkit/calculatedistance/methods/Chi2.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package nl.biopet.tools.tenxkit.calculatedistance.methods
+
+class Chi2 extends Method {
+  protected def calulateMethod(cell1: Array[Int], cell2: Array[Int]): Double = {
+    val cell1Total = cell1.sum.toDouble
+    val cell2Total = cell2.sum.toDouble
+    val total = cell1Total + cell2Total
+    val alleleTotals =
+      cell1.zip(cell2).map { case (a1, a2) => (a1 + a2).toDouble }
+    (cell1.zipWithIndex ++ cell2.zipWithIndex).map {
+      case (count, idx) =>
+        val expected = cell1Total * (alleleTotals(idx) / total)
+        val x = count - expected
+        (x * x) / expected
+    }.sum
+  }
+}
