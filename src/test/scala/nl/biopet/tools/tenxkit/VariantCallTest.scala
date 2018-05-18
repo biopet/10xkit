bd407463370b0ffa74b954a0ef82622f194418c3
pjvan_thof <pjrvanthof@gmail.com>
2018-05-18 13:32:13 +0200

    Auto format

--- src/test/scala/nl/biopet/tools/tenxkit/VariantCallTest.scala
+++ src/test/scala/nl/biopet/tools/tenxkit/VariantCallTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2018 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.tenxkit
 
 import nl.biopet.test.BiopetTest
@@ -7,10 +28,21 @@ import org.testng.annotations.Test
 class VariantCallTest extends BiopetTest {
   @Test
   def testUniqueAlleles(): Unit = {
-    val call1 = VariantCall(1, 1, "A", Array("C"), Map(1 -> Array(AlleleCount(2), AlleleCount(2)), 2 -> Array(AlleleCount(2), AlleleCount(2))))
+    val call1 = VariantCall(1,
+                            1,
+                            "A",
+                            Array("C"),
+                            Map(1 -> Array(AlleleCount(2), AlleleCount(2)),
+                                2 -> Array(AlleleCount(2), AlleleCount(2))))
     call1.getUniqueAlleles(Map(1 -> "group1", 2 -> "group2")) shouldBe empty
 
-    val call2 = VariantCall(1, 1, "A", Array("C"), Map(1 -> Array(AlleleCount(2), AlleleCount(2)), 2 -> Array(AlleleCount(), AlleleCount(2))))
-    call2.getUniqueAlleles(Map(1 -> "group1", 2 -> "group2")) shouldBe Array("group1" -> "A")
+    val call2 = VariantCall(1,
+                            1,
+                            "A",
+                            Array("C"),
+                            Map(1 -> Array(AlleleCount(2), AlleleCount(2)),
+                                2 -> Array(AlleleCount(), AlleleCount(2))))
+    call2.getUniqueAlleles(Map(1 -> "group1", 2 -> "group2")) shouldBe Array(
+      "group1" -> "A")
   }
 }
