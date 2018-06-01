2c68c4d9d9f874ad4722c5bc4f0f1ef91c6c0b64
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-06-01 11:04:01 +0200

    Switch sorting again

--- src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
+++ src/main/scala/nl/biopet/tools/tenxkit/variantcalls/CellVariantcaller.scala
@@ -131,7 +131,7 @@ object CellVariantcaller extends ToolCommand[Args] {
     sc.setLocalProperty("spark.scheduler.pool", "high-prio")
     val filteredVariants =
       filterVariants(allVariants, seqError, cutoffs).map(
-        _.repartition(10000).cache())
+        _.sortBy(x => (x.contig, x.pos), numPartitions = 500).cache())
 
     val writeFilterVcfFuture =
       if (writeFilteredVcf) {
@@ -139,7 +139,7 @@ object CellVariantcaller extends ToolCommand[Args] {
           Thread.sleep(1000)
           sc.setLocalProperty("spark.scheduler.pool", "low-prio")
           VariantCall.writeToPartitionedVcf(
-            rdd.sortBy(x => (x.contig, x.pos), numPartitions = 200),
+            rdd,
             new File(outputDir, "filter-vcf"),
             correctCells,
             dict,
