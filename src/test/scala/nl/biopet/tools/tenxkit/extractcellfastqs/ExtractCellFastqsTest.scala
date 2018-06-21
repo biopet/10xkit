5725e011dca2aac7af842e87e1438b1b92d70740
pjvan_thof <pjrvanthof@gmail.com>
2018-06-21 11:04:33 +0200

    Adding basic test

---
+++ src/test/scala/nl/biopet/tools/tenxkit/extractcellfastqs/ExtractCellFastqsTest.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package nl.biopet.tools.tenxkit.extractcellfastqs
+
+import java.io.File
+
+import nl.biopet.utils.test.tools.ToolTest
+import org.testng.annotations.Test
+
+class ExtractCellFastqsTest extends ToolTest[Args] {
+  def toolCommand: ExtractCellFastqs.type = ExtractCellFastqs
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ExtractCellFastqs.main(Array())
+    }
+  }
+
+  @Test
+  def testDefault(): Unit = {
+    val outputFile = File.createTempFile("test.", ".csv")
+    outputFile.delete()
+    outputFile.mkdirs()
+    outputFile.deleteOnExit()
+    val sampleTag = "SM"
+    ExtractCellFastqs.main(
+      Array(
+        "-i",
+        resourcePath("/read_bam.bam"),
+        "-R",
+        resourcePath("/reference.fasta"),
+        "--sparkMaster",
+        "local[2]",
+        "--sampleTag",
+        sampleTag,
+        "-o",
+        outputFile.getAbsolutePath,
+        "--correctCells",
+        resourcePath("/group1.txt")
+      ))
+  }
+}
