31a1254818d0646ea19e43978ee0d7f2566d12bb
pjvan_thof <pjrvanthof@gmail.com>
2018-07-02 16:01:29 +0200

    WIP

--- src/main/scala/nl/biopet/tools/tenxkit/extractgroupvariants/ExtractGroupVariants.scala
+++ src/main/scala/nl/biopet/tools/tenxkit/extractgroupvariants/ExtractGroupVariants.scala
@@ -53,10 +53,7 @@ object ExtractGroupVariants extends ToolCommand[Args] {
     val dict = sc.broadcast(fasta.getCachedDict(cmdArgs.reference))
     val correctCells = tenxkit.parseCorrectCells(cmdArgs.correctCells)
     val correctCellsMap = tenxkit.correctCellsMap(correctCells)
-    val groups = sc.broadcast(cmdArgs.groups.map {
-      case (name, file) =>
-        name -> io.getLinesFromFile(file).map(correctCellsMap.value).toArray
-    })
+    val groups = sc.broadcast(tenxkit.readClusters(cmdArgs.groupsFile).map{ case (cluster, cells) => cluster -> cells.map(correctCellsMap.value)})
     val groupsMap = sc.broadcast(groups.value.flatMap {
       case (k, l) => l.map(_ -> k)
     })
@@ -149,9 +146,7 @@ object ExtractGroupVariants extends ToolCommand[Args] {
          "--correctCells",
          "<list of cells>",
          "--group",
-         "<group name>=<group file>",
-         "--group",
-         "<group name>=<group file>"
+         "<groups file>"
        )}
       |
     """.stripMargin
